6e034d652472e280d1bfd5c8750aa9db747bb247
Nazar-GitH <barananastasia14@gmail.com>
2022-01-31 18:15:08 +0000

    chat

--- my-chat/src/App.js
+++ my-chat/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -9,7 +8,7 @@ function App() {
       </div>
       <div id="conversation-list">
         <div class="conversation active">
-          <img src="https://m.media-amazon.com/images/M/MV5BZjcyMWEzMzAtY2RjMy00NTlhLWFlYzgtZWRlODk5NmY1OTQxXkEyXkFqcGdeQXVyMzQxNTk0ODg@._V1_.jpg" alt="Josefina" />
+          <img src={require("./images/images-women-1.jpg")} alt="Josefina" />
           <div class="title-text">Josefina</div>
           <div class="created-date">now</div>
           <div class="conversation-message">
@@ -17,7 +16,7 @@ function App() {
           </div>
         </div>
       <div class="conversation">
-          <img src="https://img.freepik.com/free-photo/young-and-beautiful-woman-in-pink-warm-sweater-natural-look-smiling-portrait-on-isolated-long-hair_285396-896.jpg?size=626&ext=jpg" alt="Alice Freeman" />
+          <img src={require("./images/images-women-2.jpg")} alt="Alice Freeman" />
           <div class="title-text">Alice Freeman</div>
           <div class="created-date">1 seconds ago</div>
           <div class="conversation-message">
@@ -25,7 +24,7 @@ function App() {
           </div>
         </div>
         <div class="conversation">
-          <img src="https://c6oxm85c.cloudimg.io/cdno/n/q85/https://az617363.vo.msecnd.net/imgmodels/c0600498-748f-4bcf-9952-a6b060bd411d2afdbaf7563fcf87168f159e7727a650_thumb.jpg" alt="Velazquez" />
+          <img src={require("./images/images-men-1.jpeg")} alt="Velazquez" />
           <div class="title-text">Velazquez</div>
           <div class="created-date">1 minute ago</div>
           <div class="conversation-message">
@@ -33,7 +32,7 @@ function App() {
           </div>
         </div>
         <div class="conversation">
-          <img src="https://images.pexels.com/photos/38554/girl-people-landscape-sun-38554.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Barrera" />
+          <img src={require("./images/images-women-3.jpg")} alt="Barrera" />
           <div class="title-text">Barrera</div>
           <div class="created-date">1 hour ago</div>
           <div class="conversation-message">
@@ -41,7 +40,7 @@ function App() {
           </div>
         </div>
         <div class="conversation">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsrbJG6gMZ1o6wS97zmIdYiZ8bBBZ2Ucuqaw&usqp=CAU" alt="Izabella" />
+          <img src={require("./images/images-women-1.jpg")} alt="Izabella" />
           <div class="title-text">Izabella</div>
           <div class="created-date">1 week ago</div>
           <div class="conversation-message">
@@ -49,7 +48,7 @@ function App() {
           </div>
         </div>
         <div class="conversation">
-          <img src="https://media.istockphoto.com/photos/smiling-indian-man-looking-at-camera-picture-id1270067126?k=20&m=1270067126&s=612x612&w=0&h=ZMo10u07vCX6EWJbVp27c7jnnXM2z-VXLd-4maGePqc=" alt="John" />
+          <img src={require("./images/images-men-2.jpg")} alt="John" />
           <div class="title-text">John</div>
           <div class="created-date">1 month ago</div>
           <div class="conversation-message">
@@ -57,7 +56,7 @@ function App() {
           </div>
         </div>
         <div class="conversation">
-          <img src="https://www.c-and-a.com/marketing/c_scale,h_420,q_auto:eco,e_sharpen:70/202141/CC-Herren-Businessmode.jpg" alt="Mike" />
+          <img src={require("./images/images-men-3.jpg")} alt="Mike" />
           <div class="title-text">Mike</div>
           <div class="created-date">2 month ago</div>
           <div class="conversation-message">
@@ -65,7 +64,7 @@ function App() {
           </div>
         </div>
         <div class="conversation">
-          <img src="https://hips.hearstapps.com/hbz.h-cdn.co/assets/17/15/hbz-hottest-men-justin-timberlake-gettyimages-103303556.jpg" alt="James" />
+          <img src={require("./images/images-men-3.jpg")} alt="James" />
           <div class="title-text">James</div>
           <div class="created-date">3 month ago</div>
           <div class="conversation-message">
@@ -73,7 +72,7 @@ function App() {
           </div>
         </div>
         <div class="conversation">
-          <img src="https://nextluxury.com/wp-content/uploads/Top-75-Best-Manly-Hobbies-For-Men-1.jpg" alt="Robert" />
+          <img src={require("./images/images-men-3.jpg")} alt="Robert" />
           <div class="title-text">Robert</div>
           <div class="created-date">4 month ago</div>
           <div class="conversation-message">
@@ -86,7 +85,7 @@ function App() {
     </div>
     <div id="chat-title">
       <span>Daryl Duckmanton</span>
-      <img src="https://i.pinimg.com/736x/4b/3c/5e/4b3c5e3aa9c7353f3f8fc9017c44a072.jpg" alt="Delete Conversation" />
+      <img src={require("./images/images-men-3.jpg")} alt="Delete Conversation" />
     </div>
     <div id="chat-message-list">
       <div class="message-row you-message">
@@ -97,7 +96,7 @@ function App() {
       </div>
       <div class="message-row other-message">
         <div class="message-content">
-          <img src="https://i.pinimg.com/736x/4b/3c/5e/4b3c5e3aa9c7353f3f8fc9017c44a072.jpg" alt="Daryl Duckmanton" />
+          <img src={require("./images/images-men-2.jpg")} alt="Daryl Duckmanton" />
           <div class="message-text">
             Yeah I think it is best we do that. Otherwise things won't work well at all.
           </div>
@@ -114,7 +113,7 @@ function App() {
           </div>
           <div class="message-row other-message">
             <div class="message-content">
-              <img src="https://i.pinimg.com/736x/4b/3c/5e/4b3c5e3aa9c7353f3f8fc9017c44a072.jpg" alt="Daryl Duckmanton" />
+              <img src={require("./images/images-men-2.jpg")} alt="Daryl Duckmanton" />
               <div class="message-text">
                 All I know is where I live it's too hard
                 to record because of all the street noise
@@ -133,7 +132,7 @@ function App() {
           </div>
           <div class="message-row other-message">
             <div class="message-content">
-              <img src="https://i.pinimg.com/736x/4b/3c/5e/4b3c5e3aa9c7353f3f8fc9017c44a072.jpg" alt="Daryl Duckmanton" />
+              <img src={require("./images/images-men-2.jpg")} alt="Daryl Duckmanton" />
               <div class="message-text">
                 I am just in the process of finishing off the 
                 last pieces of material for the cource.
@@ -151,7 +150,7 @@ function App() {
           </div>
           <div class="message-row other-message">
             <div class="message-content">
-              <img src="https://i.pinimg.com/736x/4b/3c/5e/4b3c5e3aa9c7353f3f8fc9017c44a072.jpg" alt="Daryl Duckmanton" />
+              <img src={require("./images/images-men-2.jpg")} alt="Daryl Duckmanton" />
               <div class="message-text">
                 Hey mate what's up?
               </div>
@@ -168,7 +167,7 @@ function App() {
           </div>
         </div>
         <div id="chat-form">
-          <img src="https://www.iconpacks.net/icons/2/free-attachment-icon-1483-thumb.png" alt="Add Attachment" />
+          <img src={require("./images/images-men-2.jpg")} alt="Add Attachment" />
           <input type="text" placeholder="type a message" />
         </div>
       </div>
