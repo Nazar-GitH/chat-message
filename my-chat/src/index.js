77b83b8fb533e8564262ac26d0123e4c50871b6c
Nazar-GitH <barananastasia14@gmail.com>
2022-02-04 17:50:01 +0000

    chat

--- my-chat/src/index.js
+++ my-chat/src/index.js
@@ -1,12 +1,26 @@
-import './index.css';
 import reportWebVitals from './reportWebVitals';
-import {rerenderEntireTree} from './render'
-import state from './redux/state'
+import state, { subscribe } from './redux/state';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { addMessage } from './redux/state';
+import { BrowserRouter } from 'react-router-dom'
 
 
+let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <React.StrictMode>
+                <App state={state} addMessage={addMessage} />
+            </React.StrictMode>
+        </BrowserRouter>,
+        document.getElementById('root')
+    );
+}
 
-rerenderEntireTree(state)
-
+rerenderEntireTree(state);
+subscribe(rerenderEntireTree)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- my-chat/src/redux/state.js
+++ my-chat/src/redux/state.js
@@ -1,5 +1,6 @@
-import { rerenderEntireTree } from "../render"
+let rerenderEntireTree = () => {
 
+}
 let state = {
      dialogsData: [
         {src: 'https://s1.1zoom.me/big0/700/Gray_background_Brunette_girl_Hair_Smile_Glance_563423_1280x872.jpg', text: 'You are the worst', data: '1 hour ago' }
@@ -12,7 +13,7 @@ let state = {
       ]
 }
 
- export let addMessage = (dialogsData) => {
+ export const addMessage = (dialogsData) => {
   let newMessage = {
     src: "https://media.istockphoto.com/photos/real-woman-picture-id1198027123?b=1&k=20&m=1198027123&s=170667a&w=0&h=aXmAB1C93akXzJlXQx6ldCVT3I3nUReQHT-myoCxmCI=" ,
     text: dialogsData,
@@ -20,6 +21,11 @@ let state = {
   }
   state.dialogsData.push(newMessage)
   rerenderEntireTree(state)
+}
+
+export const subscribe = (observer) => {
+  rerenderEntireTree = observer;
+
 }
   
 export default state
